10171abf1b83669b9e3a2d3dc551c3b42581346f
kartop04 <47423164+kartop04@users.noreply.github.com>
2020-04-30 08:53:43 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Team Fixit`,
   },
   plugins: [
     {
@@ -26,7 +26,7 @@ module.exports = {
       options: {
         name: `Cara - @lekoarts/gatsby-theme-cara`,
         short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        description: `Description: here`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
